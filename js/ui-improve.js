d5d5de7b6b9599df5bccbfb2562ae532a72532b9
Cello <lyfllrr@gmail.com>
2023-12-12 21:17:30 +0900

    add hoveronscrollfunction on mobile device

--- js/ui-improve.js
+++ js/ui-improve.js
@@ -1,27 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// //阅读方向指示
-    // var directionGuider = document.getElementById("directionGuider");
+    // var directionGuider = document.getElementById('directionGuider');
 	// directionGuider.onclick = function(){
 	// 	directionGuider.parentNode.nextElementSibling.childNodes[1].click();
 	// }
 	// // 画册方向转换
-	// var gallery = document.getElementById("my-gallery");
+	// var gallery = document.getElementById('my-gallery');
 	// if (gallery.getAttribute('readfromright')){
-	// 	gallery.style.flexDirection = "row-reverse"
+	// 	gallery.style.flexDirection = 'row-reverse'
 	// }
 	
 	//根据首图尺寸设定本篇图片尺寸
-	let mangaBlocks = document.getElementsByClassName("mangaBlocks");
+	setImageSize();
+
+	//移动端根据屏幕高度触发hover并禁用默认hover
+	if (document.hasOwnProperty('ontouchstart')) {
+		focusOnCenterDOM();
+		stopDefaultHoverEffect();
+	}
+})
+
+function setImageSize(){
+	let mangaBlocks = document.getElementsByClassName('mangaBlocks');
 	if (mangaBlocks.length>0){	
 		let img = new Image();
 		img.src=mangaBlocks[0].href;
 		img.onload =function(){
 			for (let i=0;i<mangaBlocks.length;i++){
-				mangaBlocks[i].setAttribute("data-pswp-width", img.width);
-				mangaBlocks[i].setAttribute("data-pswp-height", img.height);
+				mangaBlocks[i].setAttribute('data-pswp-width', img.width);
+				mangaBlocks[i].setAttribute('data-pswp-height', img.height);
 			}
 		}
 	}
+}
 
-})
+function focusOnCenterDOM(){
+	let posts = document.querySelectorAll('.post-preview');
+	window.addEventListener('scroll',function(){
+		posts.forEach(function(post){
+			if(	post.getBoundingClientRect().top < window.innerHeight*0.4 && post.nextElementSibling.getBoundingClientRect().bottom >= window.innerHeight*0.4 ){
+				post.classList.add('active');
+			} else {
+				post.classList.remove('active');
+			}	
+		});
+	});
+	
+}
+
+function stopDefaultHoverEffect(){
+	let posts = document.querySelectorAll('.post-preview');
+	posts.forEach(function(post){
+		post.classList.remove('hoverable');
+	})
+}
